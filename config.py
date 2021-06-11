1e2d80b9cc178082c0c366c5e18077d0f7c25cd3
isoteric <rephrakt@riseup.net>
2021-06-11 11:36:41 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# Chir.PY
+# config.py
+
+class api:
+	consumer_key        = 'CHANGEME'
+	consumer_secret     = 'CHANGEME'
+	access_token        = 'CHANGEME'
+	access_token_secret = 'CHANGEME'
+
+class throttle:
+	favorite = 75
+	follow   = 75
+	message  = 750
+	tweet    = 750
+	unfollow = 75
+
+class settings:
+	keywords = ['500aday','autofollow','autofollowback','f4f','follow','follow4follow','followback','followtrain','instantfollow','instantfollowback','teamfollowback','wefollowback']
+	message  = 'Thank you for following our Twitter account!' # Set to None to disable sending messages to new followers
+	woeid    = 23424975 # Where On Earth ID (http://www.woeidlookup.com/)
